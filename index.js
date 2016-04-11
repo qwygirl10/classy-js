c86a00ddb6b7978afae9ea4f1daf081014bd7727
wenyuqian <qwy.sakura@gmail.com>
2016-04-11 22:49:14 +0800

    super super super

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-function Class(obj) {
-	var A = function() {};
+var createClass = function(obj) {
+	var fun = function() {};
 	for (var prop in obj) {
 		if( obj.hasOwnProperty( prop ) ) {
 			if(prop === 'initialize') {
-				A = obj[prop];
+				fun = obj[prop];
 			}else {
-				A.prototype[prop] = obj[prop];
+				fun.prototype[prop] = obj[prop];
 			}
 		}
 	}
-	return A;
+
+	return fun;
+}
+
+
+var Class = function(child, parent) {
+	parent = parent || Object;
+	child = createClass(child);
+
+	if(parent) {
+		child.prototype.__proto__ = new parent();
+		child.prototype.constructor  = child;
+
+		child.__super__ = parent;
+		var recursiveSuperClass = child;
+		child.prototype.super = function() {
+			var args = Array.prototype.splice.call(arguments, 1);
+			if(recursiveSuperClass.__super__ !== Object) {
+				recursiveSuperClass = recursiveSuperClass.__super__;
+			}else {
+				recursiveSuperClass = parent;
+			}
+			return recursiveSuperClass.prototype[arguments[0]].apply(this, args);
+		};
+	}else {
+		child.__super__ = Object;
+	}
+
+	
+
+	return child;
+
 };
 
 module.exports = Class;
